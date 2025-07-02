7555ec4cf09958fc4f01b8915247b96580bd1950
harusora <longcnhe141293@gmail.com>
2025-07-02 16:26:08 +0700

    add config

--- server.js
+++ server.js
@@ -31,4 +31,6 @@ fs.readdirSync(routesPath).forEach((file) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT} 🚀`);
+});
